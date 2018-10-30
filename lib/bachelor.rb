e00801aa2047415ab25bd9bbb10e82b76a4d3657
Robin Kim <rkim1214@gmail.com>
2018-10-30 14:21:54 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,8 +3,10 @@ require "pry"
 def get_first_name_of_season_winner(data, season)
   data[season].each do |contestant_info|
     if contestant_info["status"] == "Winner"
-      full_name = contestant_info["name"].split(" ").first
+      winner_first_name = contestant_info["name"].split(" ").first
+    end 
   end 
+  winner_first_name
 end
 
 def get_contestant_name(data, occupation)
