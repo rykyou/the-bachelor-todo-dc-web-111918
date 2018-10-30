ebb689332d1e6d945b6a7075e35de4508a34650a
Robin Kim <rkim1214@gmail.com>
2018-10-30 14:17:03 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,7 +1,7 @@
 require "pry"
 
 def get_first_name_of_season_winner(data, season)
-  data.each do |examining_season, 
+  data.each do |examining_season, contestant_info|
     binding.pry
   end 
 end
