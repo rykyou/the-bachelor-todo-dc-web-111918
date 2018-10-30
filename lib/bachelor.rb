ac8fdc9edcedacfb7d65e4237793b0230bb346f4
Robin Kim <rkim1214@gmail.com>
2018-10-30 14:25:50 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -12,7 +12,11 @@ end
 
 def get_contestant_name(data, occupation)
   contestant_with_occupation = ""
-  data.each do |
+  data.each do |season, contestant_array|
+    contestant_array.each do |contestant_info|
+      binding.pry
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
